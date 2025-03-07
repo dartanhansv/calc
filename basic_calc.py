e83370152631e052efc2b82ee66c950b51e7d537
Dartanhan S. Venturini <dartanhan@outlook.com>
2025-03-06 22:46:38 -0300

    add basic calc

---
+++ basic_calc.py
@@ -0,0 +1,33 @@
+'''
+A calculator that performs basic arithmetic operations
+'''
+
+# Greating
+print("Welcome to the Basic_Calc!")
+
+# Get numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Get the operation
+print("Choose an arithmetic operation: (+), (-), (*), or (/)")
+operation = input("Enter the operation: ")
+
+# Perform the calculation
+if operation == "+":
+    result = num1 + num2
+    print(f"The result is: {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"The result is: {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"The result is: {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"The result is: {result}")
+    else:
+        print("Error: Division by zero is not allowed!")
+else:
+    print("Invalid operation")
